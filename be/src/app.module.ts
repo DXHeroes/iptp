49bd4977f3bab78845f4aee3e22967a06786f7ea
ryzzaki <cuong.nugi@gmail.com>
2020-10-31 13:14:44 +0100

    chore: add typeorm config import

--- be/src/app.module.ts
+++ be/src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
+import { ConfigService } from './config/config.service';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forRootAsync({ useClass: ConfigService }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
