7f9ca5bc4bf96e2ab961799e5128a48a36959804
alex.nguyen <alex.nguyen@applifting.cz>
2020-10-31 19:04:50 +0100

    feat: i sold my soul

--- fe/src/pages/Home.tsx
+++ fe/src/pages/Home.tsx
@@ -1,22 +1,25 @@
 import React from 'react';
-import Button from '../components/Button';
-import Layout from '../components/Layout';
+import { ReactComponent as Logo } from "../assets/icons/bankID.svg"
 
 interface Props {}
 
 const Home: React.FC<Props> = () => {
   return (
-    <Layout>
-      <div className="flex h-full">
-        <div className="m-auto">
-          <Button>
-            <a href="http://localhost:3000/api/auth/linkedin">
-              Login with BANK ID
-            </a>
-          </Button>
-        </div>
+    <div className="min-h-screen flex flex-col w-full bg-greylight">
+      <header className="bg-white border-b-2 border-grey-300 w-full px-20 py-10">
+        <nav className="mx-auto max-w-80rem w-full">
+          <Logo className="h-60"/>
+        </nav>
+      </header>
+      <div className="flex-1 flex">
+        <button className="m-auto border-4 border-black font-bold px-60 py-20 rounded-lg text-60">
+          <a href="http://localhost:3000/api/auth/linkedin" className="flex items-center">
+            Login with
+            <Logo className="ml-20 h-100"/>
+          </a>
+        </button>
       </div>
-    </Layout>
+    </div>
   );
 };
 
