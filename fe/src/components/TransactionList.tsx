7ce79202eab93b101cdcb1c08eca10a808c522b3
alex.nguyen <alex.nguyen@applifting.cz>
2020-11-01 09:07:29 +0100

    sadad

--- fe/src/components/TransactionList.tsx
+++ fe/src/components/TransactionList.tsx
@@ -7,20 +7,12 @@ interface Props {
   data: any;
 }
 
-// const tags = [
-//   {
-//     name: 'flow1',
-//     id: 1,
-//   },
-//   {
-//     name: 'flow2',
-//     id: 2,
-//   },
-//   {
-//     name: 'flow3',
-//     id: 3,
-//   },
-// ];
+const tags = [
+  {
+    name: 'flow1',
+    id: 1,
+  },
+];
 
 const TransactionList: React.FC<Props> = ({ data }) => (
   <div>
@@ -44,15 +36,15 @@ const TransactionList: React.FC<Props> = ({ data }) => (
               <div className="text-grey mt-5 text-14">
                 {moment(transaction.date).format('D MMM YYYY, HH:mm ')}
               </div>
-              {/* <div className="flex mt-10">
-              {tags.map((tag) => (
-                <Link key={tag.id} to={`/flow/${tag.id}`}>
-                  <div className="text-13 font-medium text-grey bg-greylight py-5 px-10 mr-10 rounded-full">
-                    {tag.name}
-                  </div>
-                </Link>
-              ))}
-            </div> */}
+              <div className="flex mt-10">
+                {tags.map((tag) => (
+                  <Link key={tag.id} to={`/flow/${tag.id}`}>
+                    <div className="text-13 font-medium text-grey bg-greylight py-5 px-10 mr-10 rounded-full">
+                      {tag.name}
+                    </div>
+                  </Link>
+                ))}
+              </div>
             </div>
             <div className="ml-auto pr-20">{`${transaction.amount.value} ${transaction.amount.currency}`}</div>
           </li>
