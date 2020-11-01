7ed38fa2ceb528735f3c80fed1246035eda204d0
ryzzaki <cuong.nugi@gmail.com>
2020-11-01 08:01:51 +0100

    fix: flow by id

--- fe/src/utils/api.ts
+++ fe/src/utils/api.ts
@@ -10,5 +10,8 @@ export const fetchTransactions = async (id: string) =>
 export const fetchFlows = async () =>
   axios.get('http://localhost:3000/api/flows');
 
+export const fetchFlowById = async (id: string) =>
+  axios.get(`http://localhost:3000/api/flows/${id}`);
+
 export const createFlow = async (data: FlowState) =>
   axios.post('http://localhost:3000/api/flows/create', data);
